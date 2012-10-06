b5744632ce9ea3ed693549f0cfa10147763e8ba6
Никита Володин [Nikita Volodin] <volodin.n@gmail.com>
2012-10-05 23:05:31 -0300

    Add comments into code

--- PP/5.4/src/HiLoGame.java
+++ PP/5.4/src/HiLoGame.java
@@ -13,45 +13,55 @@ public class HiLoGame
     {
         Scanner scan = new Scanner(System.in);
         Random rnd = new Random();
-        int guessedNumber, userGuess, guessQuantity; //random number, how many time user guesses
-        String input; //user input
-        boolean repeat; //repeat game or halt game
+        int guessedNumber, userGuess = 0, guessQuantity = 0; //random number, how many time user guesses
+        String input = ""; //user input
+        boolean repeat = false; //repeat game or halt game
         
         do
         {
-            guessedNumber = rnd.nextInt(100) + 1;
-            guessQuantity = 0;
-            input = "";
-            repeat = false;
+            //guessedNumber = rnd.nextInt(100) + 1;
+            guessedNumber = 50;
             
             do
             {
-                System.out.println("Guess number from 1 to 100? (Write 'q' to exit)");
+                //user inputs number
+                System.out.print("Guess number from 1 to 100? (Write 'q' to exit): ");
                 input = scan.nextLine();
                 System.out.println();
                 
-                userGuess = Integer.parseInt(input);
-                if (userGuess != guessedNumber)
+                //make int from string if it is not q
+                if (!input.equals("q"))
                 {
-                    guessQuantity++;
-                    if (userGuess < guessedNumber)
+                    //try to convert
+                    userGuess = Integer.parseInt(input);
+                    
+                    //if user didn't guess then inc quantity and say lower or higher
+                    if (userGuess != guessedNumber)
                     {
-                        System.out.println("Higher");
-                    }
-                    else if (userGuess > guessedNumber)
-                    {
-                        System.out.println("Lower");
+                        guessQuantity++;
+                        if (userGuess < guessedNumber)
+                        {
+                            System.out.println("Higher");
+                        }
+                        else if (userGuess > guessedNumber)
+                        {
+                            System.out.println("Lower");
+                        }
                     }
                 }
+            //if user did not guess or if input not "q" then we repeat
             } while (userGuess != guessedNumber || !input.equals("q"));
             
+            //if we don't exit with input "q" from prev while then do this
             if (!input.equals("q"))
             {
+                //if user guessed then we print out message and quantity of guesses
                 if (userGuess == guessedNumber)
                 {
                     System.out.println("Congratulations! You win. You guessed: " + 
                             guessQuantity + " times.");
                 }
+                //ask user if he/she wants to play one more time
                 System.out.print("Do you want to play one more time? (y/n): ");
                 input = scan.nextLine();
                 if (input.equals("y") || input.equals("Y") ||
