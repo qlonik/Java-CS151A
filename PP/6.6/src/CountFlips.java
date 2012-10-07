fb2d2faec6194eaf3060a86c4b0110eb167f7aff
Никита Володин [Nikita Volodin] <volodin.n@gmail.com>
2012-10-07 14:57:41 -0300

    Programming project 6.6

---
+++ PP/6.6/src/CountFlips.java
@@ -0,0 +1,31 @@
+/**
+ * CountFlips.java
+ * @author user
+ * 
+ * Programming project 6.6 page 296
+ * 
+ * class flips coin 100 times and counts how many each side appears
+ */
+public class CountFlips
+{
+    public static void main(String[] args)
+    {
+        Coin coin = new Coin();
+        int heads = 0, tails = 0;
+        
+        for (int i = 0; i < 100; i++)
+        {
+            coin.flip();
+            if (coin.isHeads())
+            {
+                heads++;
+            }
+            else
+            {
+                tails++;
+            }
+        }
+        
+        System.out.println("Heads: " + heads + "\nTails: " + tails);
+    }
+}
