3d64d1f8401d5f77cea93da0a8af73a23083ceeb
Никита Володин [Nikita Volodin] <volodin.n@gmail.com>
2012-10-11 00:41:52 -0300

    Created class DeckOfCards that represents deck of cards with a size of 52 cards. Class includes getters for face value and/or suit of a card at some position.

---
+++ PP/6.24/src/DeckOfCards.java
@@ -0,0 +1,52 @@
+
+/**
+ * DeckOfCards.java
+ *
+ * @author Nikita Volodin
+ *
+ * Programming project 6.24 page 299
+ *
+ * This class represents deck of 52 standard cards
+ */
+public class DeckOfCards {
+
+    private final int SIZE_OF_DECK = 52;
+    private Card[] deck = new Card[SIZE_OF_DECK];
+
+    //constructor
+    public DeckOfCards() {
+        int pos = 0;
+
+        for (int faceValue = 0; faceValue < Card.getFACE_VALUES().length; faceValue++) {
+            for (int suit = 0; suit < Card.getSUITS().length; suit++) {
+                deck[pos] = new Card(Card.getFACE_VALUES()[faceValue], Card.getSUITS()[suit]);
+                pos++;
+            }
+        }
+    }
+
+    //get size of a deck
+    public int getSizeOfDeck() {
+        return SIZE_OF_DECK;
+    }
+
+    //get all array of cards
+    public Card[] getCards() {
+        return deck;
+    }
+
+    //get card at the position
+    public Card getCard(int position) {
+        return deck[position];
+    }
+
+    //get face value of the card at position
+    public String getFaceValue(int position) {
+        return deck[position].getFaceValue();
+    }
+
+    //get suit of the card at position
+    public String getSuit(int position) {
+        return deck[position].getSuit();
+    }
+}
