f05118d47e4f7caf071d34aa8fbf6f4574840fe8
Никита Володин [Nikita Volodin] <volodin.n@gmail.com>
2012-10-11 00:39:56 -0300

    Add constructor that creates given card and method that checks if string in array of strings

--- PP/6.24/src/Card.java
+++ PP/6.24/src/Card.java
@@ -12,15 +12,51 @@ import java.util.Random;
  */
 public class Card {
 
-    final private String[] FACE_VALUES = {"Ace", "Two", "Three", "Four", "Five",
+    private static final String[] FACE_VALUES = {"Ace", "Two", "Three", "Four", "Five",
         "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"};
-    final private String[] SUITS = {"Clubs", "Diamonds", "Hearts", "Spades"};
+    private static final String[] SUITS = {"Clubs", "Diamonds", "Hearts", "Spades"};
     private String faceValue, suit;
     Random rnd = new Random();
 
+    //constructor that creates random card
     public Card() {
         faceValue = FACE_VALUES[rnd.nextInt(FACE_VALUES.length)];
         suit = SUITS[rnd.nextInt(SUITS.length)];
+    }
+
+    //constructor that creates given card
+    public Card(String inputFaceValue, String inputSuit) {
+        if (inArray(inputFaceValue, FACE_VALUES)) {
+            faceValue = inputFaceValue;
+        } else {
+            System.out.print("Face value has to be one of this: ");
+            for (int i = 0; i < FACE_VALUES.length; i++) {
+                System.out.print(FACE_VALUES[i] + ", ");
+            }
+        }
+        if (inArray(inputSuit, SUITS)) {
+            suit = inputSuit;
+        } else {
+            System.out.print("Suit has to be one of this: ");
+            for (int i = 0; i < SUITS.length; i++) {
+                System.out.print(SUITS[i] + ", ");
+            }
+        }
+    }
+
+    //method checks if value in the array
+    private boolean inArray(String inputString, String[] inputArray) {
+        boolean result = false;
+        int pos = 0;
+
+        while (pos < inputArray.length && !result) {
+            if (inputString.equals(inputArray[pos])) {
+                result = true;
+            }
+            pos++;
+        }
+
+        return result;
     }
 
     //method gets face value of a current card
@@ -31,6 +67,16 @@ public class Card {
     //method gets suit of a current card
     public String getSuit() {
         return suit;
+    }
+
+    //method returns array of available face values
+    public static String[] getFACE_VALUES() {
+        return FACE_VALUES;
+    }
+
+    //method returns array of available suits
+    public static String[] getSUITS() {
+        return SUITS;
     }
 
     //method that represents card object as a string
