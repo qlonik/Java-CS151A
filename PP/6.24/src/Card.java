b15ecc1c6fbfc6fe4c8a774f072c6454948b4d6b
Никита Володин [Nikita Volodin] <volodin.n@gmail.com>
2012-10-10 23:45:08 -0300

    Minor formatting  fixes

--- PP/6.24/src/Card.java
+++ PP/6.24/src/Card.java
@@ -3,44 +3,38 @@ import java.util.Random;
 
 /**
  * Card.java
+ *
  * @author Nikita Volodin
- * 
+ *
  * Programming project 6.24 page 299
- * 
+ *
  * Class that represents standard card
  */
+public class Card {
 
-public class Card
-{
     final private String[] FACE_VALUES = {"Ace", "Two", "Three", "Four", "Five",
         "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"};
     final private String[] SUITS = {"Clubs", "Diamonds", "Hearts", "Spades"};
-    
     private String faceValue, suit;
-    
     Random rnd = new Random();
-    
-    public Card()
-    {
+
+    public Card() {
         faceValue = FACE_VALUES[rnd.nextInt(FACE_VALUES.length)];
         suit = SUITS[rnd.nextInt(SUITS.length)];
     }
-    
+
     //method gets face value of a current card
-    public String getFaceValue()
-    {
+    public String getFaceValue() {
         return faceValue;
     }
-    
+
     //method gets suit of a current card
-    public String getSuit()
-    {
+    public String getSuit() {
         return suit;
     }
-    
+
     //method that represents card object as a string
-    public String toString()
-    {
+    public String toString() {
         return "Face value: " + faceValue + ",\tsuit: " + suit;
     }
 }
--- PP/6.24/src/Main.java
+++ PP/6.24/src/Main.java
@@ -1,32 +1,31 @@
+
 /**
  * Main.java
+ *
  * @author Nikita Volodin
- * 
+ *
  * Programming project 6.24 page 299
- * 
+ *
  * Class that creates 5 random numbers and shows them
  */
-public class Main
-{
-    public static void main(String[] args)
-    {
+public class Main {
+
+    public static void main(String[] args) {
         //create new array of 5 cards
         Card[] cardsArr = new Card[5];
-        
-        for (int pos = 0; pos < 5; pos++)
-        {
+
+        for (int pos = 0; pos < 5; pos++) {
             //generates card in array at position 'pos'
             cardsArr[pos] = new Card();
-            
+
             int i = 0;
             while (i < pos) //Chech all cards that was generated before current
-                //where 'pos' - position of current card, 'i' - previous cards
+            //where 'pos' - position of current card, 'i' - previous cards
             {
                 //if we found that face value AND suit matches with the previous card
                 //we assume that we generate already existing card and we change it
-                while (cardsArr[pos].getFaceValue().equals(cardsArr[i].getFaceValue()) &&
-                        cardsArr[pos].getSuit().equals(cardsArr[i].getSuit()))
-                {
+                while (cardsArr[pos].getFaceValue().equals(cardsArr[i].getFaceValue())
+                        && cardsArr[pos].getSuit().equals(cardsArr[i].getSuit())) {
                     //we generate a new card at position of 'pos' - last generated card 
                     //and drop counter 'i' to 0 and start checking this 
                     //newly generated card with already existing
@@ -36,10 +35,9 @@ public class Main
                 i++;
             }
         }
-        
+
         //prints all 5 cards
-        for (int pos = 0; pos < 5; pos++)
-        {
+        for (int pos = 0; pos < 5; pos++) {
             System.out.println("Card #" + (pos + 1) + ": " + cardsArr[pos]);
         }
     }
