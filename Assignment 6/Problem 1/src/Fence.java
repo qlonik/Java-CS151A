8fa21ff0dbbe98765463dc6e2c0eeaca6c997d88
Никита Володин [Nikita Volodin] <volodin.n@gmail.com>
2012-11-05 19:43:33 -0400

    Assignment 6 Problem 1 Fence fit now if space povided less than for 11 pickets

--- Assignment 6/Problem 1/src/Fence.java
+++ Assignment 6/Problem 1/src/Fence.java
@@ -10,62 +10,69 @@ import javax.swing.JApplet;
  * This program draw a fence in the middle of an applet
  */
 public class Fence extends JApplet {
-    
-//    //window properties
-//    private final int WIDTH_OF_WINDOW = 1000;
-//    private final int HEIGHT_OF_WINDOW = 500;
 
     //fence properties
     private final int WIDE = 25;
     private final int HIGH = 300;
     private final int GAP = 20;
-    private final int NUMBER_OF_PICKETS = 11;
-    
+    private int numberOfPickets;
+
     /**
      * Method that draw fence. It called by system (as main)
+     *
      * @param page inputed by system
      */
     public void paint(Graphics page) {
+        //searching for a number of pickets if 11 is not fit
+        //we have formula: width = numberOfPickets * (WIDE + GAP) - GAP
+        //find out numberOfPickets: numberOfPickets: (width + GAP) / (WIDE + GAP)
+        int pickets = (getWidth() + GAP) / (WIDE + GAP);
+        if (pickets < 11) {
+            numberOfPickets = pickets;
+        } else {
+            numberOfPickets = 11;
+        }
+
         int topLeftX = getTopLeftX(), topLeftY = getTopLeftY();
-        
+
         Random rnd = new Random();
         Color color;
-//        setSize(new Dimension(WIDTH_OF_WINDOW, HEIGHT_OF_WINDOW));
-//        setPreferredSize(new Dimension(WIDTH_OF_WINDOW, HEIGHT_OF_WINDOW));
-        
-        for (int i = 0; i < NUMBER_OF_PICKETS; i++) {
+
+        for (int i = 0; i < numberOfPickets; i++) {
             color = new Color(rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256));
             page.setColor(color);
             page.fillRect(topLeftX + (WIDE + GAP) * i, topLeftY, WIDE, HIGH);
             page.setColor(Color.black);
             page.drawRect(topLeftX + (WIDE + GAP) * i, topLeftY, WIDE, HIGH);
         }
-        
+
         //first horiz picket
         color = new Color(rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256));
         page.setColor(color);
-        page.fillRect(topLeftX, topLeftY + 20, NUMBER_OF_PICKETS * (WIDE + GAP) - GAP, WIDE);
+        page.fillRect(topLeftX, topLeftY + 20, numberOfPickets * (WIDE + GAP) - GAP, WIDE);
         page.setColor(Color.black);
-        page.drawRect(topLeftX, topLeftY + 20, NUMBER_OF_PICKETS * (WIDE + GAP) - GAP, WIDE);
-        
+        page.drawRect(topLeftX, topLeftY + 20, numberOfPickets * (WIDE + GAP) - GAP, WIDE);
+
         //second horiz picket
         color = new Color(rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256));
         page.setColor(color);
-        page.fillRect(topLeftX, topLeftY + (HIGH - WIDE - 20), NUMBER_OF_PICKETS * (WIDE + GAP) - GAP, WIDE);
+        page.fillRect(topLeftX, topLeftY + (HIGH - WIDE - 20), numberOfPickets * (WIDE + GAP) - GAP, WIDE);
         page.setColor(Color.black);
-        page.drawRect(topLeftX, topLeftY + (HIGH - WIDE - 20), NUMBER_OF_PICKETS * (WIDE + GAP) - GAP, WIDE);
+        page.drawRect(topLeftX, topLeftY + (HIGH - WIDE - 20), numberOfPickets * (WIDE + GAP) - GAP, WIDE);
     }
-    
+
     /**
      * method returns top left X coordinate so image will be in center
+     *
      * @return X coord
      */
     private int getTopLeftX() {
-        return (getWidth() - (NUMBER_OF_PICKETS * (WIDE + GAP) - GAP)) / 2;
+        return (getWidth() - (numberOfPickets * (WIDE + GAP) - GAP)) / 2;
     }
-    
+
     /**
      * method returns top left Y coord so image will be in center
+     *
      * @return Y coord
      */
     private int getTopLeftY() {
